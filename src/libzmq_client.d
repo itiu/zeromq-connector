9f6719406e154275ee7a51b399a2fcc611015243
Itiu <ValeriyBushenev@gmail.com>
2010-09-07 22:37:35 +0400

    +1

--- src/libzmq_client.d
+++ src/libzmq_client.d
@@ -112,7 +112,7 @@ class libzmq_client: mom_client
 	}
 
 }
-/*
+
 void main(char[][] args)
 {
 	Stdout.format("main#1").newline;
@@ -142,4 +142,3 @@ char* get_message(byte* message, ulong message_size, mom_client from_client)
 	Stdout.format("[{}] data: {}", count, fromStringz(cast(char*) message)).newline;
 	return "aaaaa test message";
 }
-*/
